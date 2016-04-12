3ba21d340ca6e803a3e108ba3f6c1912b27ecf61
tcarrio <tom@carrio.me>
2016-04-12 02:31:09 -0400

    Updated DB URI

--- config/env_config.py
+++ config/env_config.py
@@ -10,7 +10,7 @@ class DevelopmentConfig(Config):
     SECRET_KEY = '@NYpQe#Bm*0iT3KcT0n%EuuGDZ^vkWA%6XSoeRqWAfOUMkLph0phbw8FKaA%VwPB'
     
 class ProductionConfig(Config):
-    DATABASE_URI = 'mysql+pymysql://nssnuser:nssnpass@nssn.carrio.me/nssndb'
+    DATABASE_URI = 'mysql+pymysql://nssnuser:2rU9ZFnd2xYMilSb@nssn.carrio.me/nssndb'
     WEB_PORT = 80
     WEB_HOST = 'nssn.carrio.me'
     SESSION_TYPE = 'memcached'
