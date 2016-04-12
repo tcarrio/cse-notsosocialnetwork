38cdb4ea8c61dd06d037f3b02f7cef89798f076e
tcarrio <tom@carrio.me>
2016-04-12 00:41:11 -0400

    Added secret key for session handling

--- config/env_config.py
+++ config/env_config.py
@@ -6,8 +6,12 @@ class DevelopmentConfig(Config):
     DATABASE_URI = 'sqlite:///tmp/nssn.db'
     WEB_PORT = 5000
     WEB_HOST = 'localhost'
+    SESSION_TYPE = 'memcached'
+    SECRET_KEY = '@NYpQe#Bm*0iT3KcT0n%EuuGDZ^vkWA%6XSoeRqWAfOUMkLph0phbw8FKaA%VwPB'
     
 class ProductionConfig(Config):
     DATABASE_URI = 'mysql+pymysql://nssnuser:nssnpass@nssn.carrio.me/nssndb'
     WEB_PORT = 80
     WEB_HOST = 'nssn.carrio.me'
+    SESSION_TYPE = 'memcached'
+    SECRET_KEY = '@NYpQe#Bm*0iT3KcT0n%EuuGDZ^vkWA%6XSoeRqWAfOUMkLph0phbw8FKaA%VwPB'
