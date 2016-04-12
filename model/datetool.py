56c5073a2b6ddc019ea6a0b63eb40f19a1c4a968
tcarrio <tom@carrio.me>
2016-04-12 02:48:09 -0400

    Removed unused files

--- model/datetool.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-class DateHelper():
-    def __init__(self):
-        self.about="I make dates for the the database. No I can't get you a date, kid"
-                
-    def get_db_date(dobd,dobm,doby):
-        print('About to parse')
-        return datetime.datetime.strptime("{}/{}/{}".format(dobd,dobm,doby), "%d/%m/%Y")
--- model/security.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-class Hasher():
-    def __init__(self):
-        self.about = "I hash passwords sent from the webpage. No puns. Sorry D:"
-    def get_hash(password=None):
-        if not password:
-            return None
-        return hashlib.sha512(password.encode('utf-8')).hexdigest()[:64]
