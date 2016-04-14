d512de075362bd6a7e625073efd3e51cfdcf0b1d
tcarrio <tom@carrio.me>
2016-04-14 00:15:04 -0400

    Updated relationship entries

--- model/database.py
+++ model/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, Sequence, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, Sequence, String, DateTime, Boolean, ForeignKey
 from config.env_config import ProductionConfig as Conf
 
 engine = create_engine(Conf.DATABASE_URI, convert_unicode=True)
@@ -35,21 +35,15 @@ class Account(Base):
     def __repr__(self):
         return '<User {}>'.format(self.name)
         
-    def validate(self):
-        return isinstance(self.account_first_name,str) and \
-            isinstance(self.account_last_name,str) and \
-            isinstance(self.account_email,str) and \
-            isinstance(self.account_password,str) and \
-            isinstance(self.account_dob,datetime) and \
-            isinstance(self.account_gender,boolean)
-
 #SQL ORM Profile Class                
 class Profile(Base):
     __tablename__ = 'profiles'
     profile_id = Column(Integer,primary_key=True)
     profile_uri = Column(String(255))
     profile_about = Column(String(1023))
-    account_email = Column(String(255))
+    
+    account_email = Column(String(255),ForeignKey('accounts.account_email'))
+    account_email_rel = relationship("Account",foreign_keys=[account_email])
     
     def __init__(self,id=None,uri=None,about=None,email=None):
         self.profile_id = id;
@@ -60,21 +54,18 @@ class Profile(Base):
     def __repr__(self):
         return '<Profile {}>'.format(self.name)
         
-    def validate(self):
-        return isinstance(self.profile_id,int) and \
-            isinstance(self.profile_uri,str) and \
-            isinstance(self.profile_about,str) and \
-            isinstance(self.account_email,str)
-            
 #SQL ORM Post Class
 class Post(Base):
     __tablename__ = 'posts'
     post_id = Column(Integer,primary_key=True)
     post_uri = Column(String(255))
-    account_email = Column(String(255))
-    receiver_email = Column(String(255))
+    account_email = Column(String(255),ForeignKey('accounts.account_email'))
+    receiver_email = Column(String(255),ForeignKey('accounts.account_email'))
     post_date = Column(DateTime)
     post_content = Column(String(4096))
+    
+    account_email_rel = relationship("Account",foreign_keys=[account_email])
+    receiver_email_rel = relationship("Account",foreign_keys=[receiver_email])
     
     def __init__(self,id=None,uri=None,pFrom=None,pTo=None,pDate=None,pContent=None):
         self.post_id = id
@@ -86,25 +77,20 @@ class Post(Base):
         
     def __repr__(self):
         return '<Post {}>'.format(self.name)
-    
-    def validate(self):
-        return isinstance(self.post_id,int) and \
-            isinstance(self.post_uri,str) and \
-            isinstance(self.account_email,str) and \
-            isinstance(self.receiver_email,str) and \
-            isinstance(self.post_date,datetime) and \
-            isinstance(self.post_content,str)
             
 #SQL ORM Message Class
 class Message(Base):
     __tablename__ = 'messages'
     message_id = Column(Integer,primary_key=True)
     message_uri = Column(String(255))
-    account_email = Column(String(255))
-    receiver_email = Column(String(255))
+    account_email = Column(String(255),ForeignKey('accounts.account_email'))
+    receiver_email = Column(String(255),ForeignKey('accounts.account_email'))
     message_date = Column(DateTime)
     message_title = Column(String(255))
     message_content = Column(String(4096))
+    
+    account_email_rel = relationship("Account",foreign_keys=[account_email])
+    receiver_email_rel = relationship("Account",foreign_keys=[receiver_email])
     
     def __init__(self,mId=None,uri=None,mFrom=None,mTo=None,mDate=None,title=None,mContent=None):
         self.message_id = mId
@@ -117,18 +103,7 @@ class Message(Base):
     
     def __repr__(self):
         return '<Message {}>'.format(self.name)
-    
-    def validate(self):
-        return isinstance(self.message_id,int) and \
-            isinstance(self.message_uri,str) and \
-            isinstance(self.account_email,str) and \
-            isinstance(self.receiver_email,str) and \
-            isinstance(self.message_date,datetime) and \
-            isinstance(self.message_title,str) and \
-            isinstance(self.message_content,str)
-            
-            
-            
+        
 def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
