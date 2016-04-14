43f00d01af2e7df5e0ee4109d3dc82aef686dc0e
tcarrio <tom@carrio.me>
2016-04-14 00:20:39 -0400

    Imports relationship

--- model/database.py
+++ model/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Sequence, String, DateTime, Boolean, ForeignKey
 from config.env_config import ProductionConfig as Conf
