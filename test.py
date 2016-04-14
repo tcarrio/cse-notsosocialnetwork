7af3519d817eaea4ffb97f9aa2a415fc9ac037d9
tcarrio <tom@carrio.me>
2016-04-14 12:26:31 -0400

    Testing new build script

--- test.py
+++ test.py
@@ -1,25 +1,30 @@
 import requests
 
-base_url = 'http://127.0.0.1/'
-test_urls = [
-    '',
-    'frontpage',
-    'home',
-    'contacts',
-    'registration',
-    'aboutus',
-    'profile',
-    'login',
-    'register',
-    'search',
-    'logout'
-]
-status_codes = []
+def get_status_codes():
+    base_url = 'http://127.0.0.1/'
+    test_urls = [
+        '',
+        'frontpage',
+        'home',
+        'contacts',
+        'registration',
+        'aboutus',
+        'profile',
+        'login',
+        'register',
+        'search',
+        'logout'
+    ]
+    status_codes = []
 
-for uri in test_urls:
-    r = requests.get('{}{}'.format(base_url,uri))
-    status_codes.append(r.status_code)
-    
+    for uri in test_urls:
+        r = requests.get('{}{}'.format(base_url,uri))
+        status_codes.append(r.status_code)
+    return status_codes
 
-# loose testing
-return (200 in status_codes)
+
+def main():
+    return (200 in get_status_codes())
+
+if __name__=="__main__":
+    main()
