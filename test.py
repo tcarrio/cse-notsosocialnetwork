644cdab577c8c563add8d6f041e4c5fff6a933fb
tcarrio <tom@carrio.me>
2016-04-14 12:21:02 -0400

    Testing new build script

---
+++ test.py
@@ -0,0 +1,26 @@
+import requests
+
+base_url = 'http://127.0.0.1/'
+test_urls = [
+    '',
+    'frontpage',
+    'home',
+    'contacts',
+    'registration',
+    'aboutus',
+    'profile',
+    'login',
+    'register',
+    'search',
+    'logout'
+]
+status_codes = []
+
+for uri in test_urls:
+    r = requests.get('{}{}'.format(base_url,uri))
+    status_codes.append(r.status_code)
+    
+
+# loose testing
+if 200 in status_codes:
+    return 1
