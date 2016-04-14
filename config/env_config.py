5b76a9d485a8d43c452ec6a66fc133acdab00ab2
tcarrio <tom@carrio.me>
2016-04-14 11:58:29 -0400

    Testing new build script

--- config/env_config.py
+++ config/env_config.py
@@ -15,4 +15,4 @@ class ProductionConfig(Config):
     WEB_HOST = 'nssn.carrio.me'
     SESSION_TYPE = 'memcached'
     SECRET_KEY = '@NYpQe#Bm*0iT3KcT0n%EuuGDZ^vkWA%6XSoeRqWAfOUMkLph0phbw8FKaA%VwPB'
-    EXT_IP = 'nssn.carrio.me'
+    EXT_IP = '127.0.0.1'
